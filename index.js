b031ad1ebb8be4514e6df3dc68631c14904c1ce0
Zakarias Bergcrona <zb222cw@student.lnu.se>
2023-10-25 11:34:50 +0200

    Tries to implement checkWin

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-    gridInit();
-});
-
 const gridContainer = document.querySelector('.gridContainer');
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    gridInit();
+    
+
+});
+
+
+let currentPlayer = 'red'
+const board = [];
 function gridInit() {
-    const board = [];
     for (let i = 0; i < 4; i++) {
         board.push([]);
         for (let j = 0; j < 4; j++) {
             board[i][j] = 0;
+            console.log(board)
             const cell = document.createElement('div');
             cell.classList.add('cell');
             gridContainer.appendChild(cell);
+            cell.addEventListener('click', () => {
+                   // checkWin()
+                if (board[i][j] === 0) {
+                    board[i][j] = currentPlayer
+                    cell.style.backgroundColor = currentPlayer
+                    currentPlayer = currentPlayer === 'red' ? 'blue' : 'red'
+                }
+            });
         }
+    }
+}
+
+function checkWin () {
+    let counter = 0
+    for (let i = 0; i < 4; i++) {
+      for(let j = 0; j < 4; i++) {
+        console.log(board[i][j])
+        if (board[i][j].backgroundColor === currentPlayer) {
+            counter++
+        } else {
+            counter = 0
+        }
+        console.log(counter)
+      }
+      
     }
 }
