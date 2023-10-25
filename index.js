40ee1cc15d0384c34a1f5fe1d275f9ec546f2b0f
Cornelia Eskilsson <ce223ex@student.lnu.se>
2023-10-25 10:41:07 +0200

    Fix board

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    gridInit()
-})
+    gridInit();
+});
 
-const gridContainer = document.querySelector('.gridContainer')
+const gridContainer = document.querySelector('.gridContainer');
 
 function gridInit() {
-    
-    for (let i =0 ; i < 16 ; i ++) {
-        const cell = document.createElement('div')
-        cell.classList.add('cell')
-
-        gridContainer.appendChild(cell)
+    const board = [];
+    for (let i = 0; i < 4; i++) {
+        board.push([]);
+        for (let j = 0; j < 4; j++) {
+            board[i][j] = 0;
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            gridContainer.appendChild(cell);
+        }
     }
 }
